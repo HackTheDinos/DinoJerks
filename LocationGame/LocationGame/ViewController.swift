05670db97eaa2ca873950830e3374eb75d8795ac
Benjamin Bojko <ben@potiondesign.com>
2015-11-21 18:37:56 -0500

    Removed ignored files

--- LocationGame/LocationGame/ViewController.swift
+++ LocationGame/LocationGame/ViewController.swift
@@ -12,7 +12,7 @@ import CoreLocation
 class ViewController : UIViewController, CLLocationManagerDelegate, UITableViewDelegate, UITableViewDataSource {
 
 	let locationManager = CLLocationManager()
-	var beacons = Dictionary<CLBeacon, CLBeaconRegion>()
+	var region = CLBeaconRegion(proximityUUID: NSUUID(UUIDString: "B7D1027D-6788-416E-994F-EA11075F1765")!, identifier: "test")
 	
 	override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
 		super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
@@ -25,11 +25,12 @@ class ViewController : UIViewController, CLLocationManagerDelegate, UITableViewD
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		locationManager.delegate = self
-		
+		locationManager.startMonitoringForRegion(region)
+		locationManager.startRangingBeaconsInRegion(region)
 	}
 	
 	func locationManager(manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], inRegion region: CLBeaconRegion) {
-		
+		print("beacons \(beacons)")
 	}
 	
 	func numberOfSectionsInTableView(tableView: UITableView) -> Int {
